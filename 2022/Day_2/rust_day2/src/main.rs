a421ef23467c21087ff2d1ee1383c94e6dd82a43
sergious234 <sergious234@gmail.com>
2022-12-03 00:59:53 +0100

    Blazingly fast

--- 2022/Day_2/rust_day2/src/main.rs
+++ 2022/Day_2/rust_day2/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::collections::HashMap;
 
 const ROCK_POINTS: u32 = 1;
 const PAPER_POINTS: u32 = 2;
@@ -11,36 +12,29 @@ const WIN: u32 = 6;
 fn read_data() -> String {
     let mut reader = std::io::BufReader::new(std::fs::File::open("src/data.txt").unwrap());
     let mut data = String::new();
-    reader.read_to_string(&mut data);
+    reader.read_to_string(&mut data).unwrap();
     data
 }
+
 
 fn main() {
     let games = read_data();
     let mut points = 0;
+    let games_possibilities: HashMap<(&str, &str), u32> = HashMap::from([
+        (("A", "X"), LOST + SCISSORS_POINTS),
+        (("A", "Y"), DRAW + ROCK_POINTS),
+        (("A", "Z"), WIN + PAPER_POINTS),
+        (("B", "X"), LOST + ROCK_POINTS),
+        (("B", "Y"), DRAW + PAPER_POINTS),
+        (("B", "Z"), WIN + SCISSORS_POINTS),
+        (("C", "X"), LOST + PAPER_POINTS),
+        (("C", "Y"), DRAW + SCISSORS_POINTS),
+        (("C", "Z"), WIN + ROCK_POINTS),
+    ]);
     
-    //  X -> Lost
-    //  Y -> Draw
-    //  Z -> Win
 
     for game in games.split("\n") {
-        points += match game.split_once(" ").unwrap() {
-            ("A", "X") => LOST + SCISSORS_POINTS,
-            ("A", "Y") => DRAW + ROCK_POINTS,
-            ("A", "Z") => WIN + PAPER_POINTS,
-
-            ("B", "X") => LOST + ROCK_POINTS,
-            ("B", "Y") => DRAW + PAPER_POINTS,
-            ("B", "Z") => WIN + SCISSORS_POINTS,
-
-            ("C", "X") => LOST + PAPER_POINTS,
-            ("C", "Y") => DRAW + SCISSORS_POINTS,
-            ("C", "Z") => WIN + ROCK_POINTS,
-            _ => {
-                println!("WTF");
-                0
-            }
-        }
+        points += games_possibilities.get(&game.split_once(" ").unwrap()).unwrap_or_else(|| {println!("WTF"); &0});
     }
 
     println!("{points}")
