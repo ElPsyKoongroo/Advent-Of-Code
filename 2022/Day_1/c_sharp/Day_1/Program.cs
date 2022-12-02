f39f45f45959ef55d6afb1bdfb3957b24823ac80
Adrian <arrodriguez1809@gmail.com>
2022-12-02 22:00:47 +0100

    Advent

---
+++ 2022/Day_1/c_sharp/Day_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Day_1;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var relativePath = "../../AOCinput";
+        var input = File.ReadAllLines(relativePath);
+
+        int maxElf = -1;
+        int actualElf = 0;
+
+        foreach(var snack in input)
+        {
+            if(string.IsNullOrEmpty(snack.Trim()))
+            {
+                if(actualElf > maxElf)
+                {
+                    maxElf = actualElf;
+                }
+                actualElf = 0;
+                continue;
+            }
+            actualElf += int.Parse(snack.Trim());
+        }
+
+        maxElf = actualElf > maxElf ? actualElf : maxElf;
+        
+
+        System.Console.WriteLine(maxElf);
+
+        return;
+    }
+}
