9d284c9cce1cfd890c3e32bf79662c931b584cd4
sergious234 <sergious234@gmail.com>
2022-12-02 23:23:14 +0100

    Day2 Par2 rust

--- 2022/Day_2/rust_day2/src/main.rs
+++ 2022/Day_2/rust_day2/src/main.rs
@@ -18,19 +18,24 @@ fn read_data() -> String {
 fn main() {
     let games = read_data();
     let mut points = 0;
+    
+    //  X -> Lost
+    //  Y -> Draw
+    //  Z -> Win
+
     for game in games.split("\n") {
         points += match game.split_once(" ").unwrap() {
-            ("A", "X") => DRAW + ROCK_POINTS,       
-            ("A", "Y") => WIN + PAPER_POINTS,  
-            ("A", "Z") => LOST + SCISSORS_POINTS,  
+            ("A", "X") => LOST + SCISSORS_POINTS,
+            ("A", "Y") => DRAW + ROCK_POINTS,
+            ("A", "Z") => WIN + PAPER_POINTS,
 
             ("B", "X") => LOST + ROCK_POINTS,
             ("B", "Y") => DRAW + PAPER_POINTS,
             ("B", "Z") => WIN + SCISSORS_POINTS,
 
-            ("C", "X") => WIN + ROCK_POINTS,
-            ("C", "Y") => LOST + PAPER_POINTS,
-            ("C", "Z") => DRAW + SCISSORS_POINTS,
+            ("C", "X") => LOST + PAPER_POINTS,
+            ("C", "Y") => DRAW + SCISSORS_POINTS,
+            ("C", "Z") => WIN + ROCK_POINTS,
             _ => {
                 println!("WTF");
                 0
