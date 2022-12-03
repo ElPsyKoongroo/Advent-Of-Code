9148ff193aae249b22012da2e8b5eb74cd538d40
Adrian <arrodriguez1809@gmail.com>
2022-12-03 14:15:55 +0100

    Day 3

---
+++ 2022/Day_3/c_sharp/Day_3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Program;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Day_3 day = new();
+        day.Answer1();
+    }
+}
+
+public class Day_3
+{
+    string[] inputLines;
+    public Day_3()
+    {
+        inputLines  = File.ReadAllLines("../../AOCinput");
+    }
+
+    public void Answer1()
+    {
+        int sum = 0;
+
+        foreach(var line in inputLines)
+        {
+            var badSnack = line.FirstOrDefault(x=> line.Count(y=> y == x) == 2, '-');
+            if(badSnack == '-')
+            {
+                continue;
+            } 
+                
+
+            int badSnackInt = badSnack >= 'a' ? badSnack - 'a' + 1 : badSnack - 'A' + 27;
+            sum += badSnackInt;
+        }
+
+        System.Console.WriteLine(sum);
+    }
+}
