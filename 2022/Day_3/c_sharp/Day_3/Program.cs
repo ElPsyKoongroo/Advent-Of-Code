b62a9ffc8806325fab12d758562b3d713d74c788
Adrian <arrodriguez1809@gmail.com>
2022-12-03 14:23:05 +0100

    Day_3 csharp

--- 2022/Day_3/c_sharp/Day_3/Program.cs
+++ 2022/Day_3/c_sharp/Day_3/Program.cs
@@ -5,7 +5,7 @@ public static class Program
     public static void Main()
     {
         Day_3 day = new();
-        day.Answer1();
+        day.Answer2();
     }
 }
 
@@ -23,12 +23,33 @@ public class Day_3
 
         foreach(var line in inputLines)
         {
-            var badSnack = line.FirstOrDefault(x=> line.Count(y=> y == x) == 2, '-');
-            if(badSnack == '-')
-            {
-                continue;
-            } 
-                
+            var line0 = line.Substring(0, line.Length/2);
+            var line1 = line.Substring(line.Length/2);
+
+
+            var badSnack = line0.First(x=> line1.Count(y=> y == x) > 0);              
+
+            int badSnackInt = badSnack >= 'a' ? badSnack - 'a' + 1 : badSnack - 'A' + 27;
+            sum += badSnackInt;
+        }
+
+        System.Console.WriteLine(sum);
+    }
+
+    public void Answer2()
+    {
+        int sum = 0;
+
+        var groupedLines = inputLines.Chunk(3).ToList();
+
+        foreach(var lines in groupedLines)
+        {
+            var badSnack = lines[0]
+                .First(
+                    x=>    
+                    lines[1].Count(y=> y == x) > 0 &&
+                    lines[2].Count(y=> y == x) > 0
+                    );              
 
             int badSnackInt = badSnack >= 'a' ? badSnack - 'a' + 1 : badSnack - 'A' + 27;
             sum += badSnackInt;
