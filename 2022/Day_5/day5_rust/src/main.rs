2d7fa56cc694d4c7f84f871f3ce9fdf4acda1959
Adrian <arrodriguez1809@gmail.com>
2022-12-05 20:03:03 +0100

    2022 Day_5 rust complete

--- 2022/Day_5/day5_rust/src/main.rs
+++ 2022/Day_5/day5_rust/src/main.rs
@@ -48,9 +48,56 @@ fn part_1(data: &str){
 
 }
 
+fn part_2(data: &str){
+    let mut crates: Vec<VecDeque<char>> = Vec::new();
+    for _ in 0..9 {
+        crates.push(VecDeque::new());
+    }
+
+
+    for line in data.split("\n").take(8){        
+        let mut i = 1;
+        let chars = line.chars().collect::<Vec<char>>();
+        
+        for n in 0..9 {
+            if !chars[i].is_whitespace() {
+                crates[n].push_front(chars[i]);
+            }
+            i += 4;
+        }
+    }
+    
+    for order in data.split("\n").skip(10) {
+        let words = order.split(" ").collect::<Vec<&str>>();
+        let cuantity = words[1].parse::<i32>().unwrap_or(0);
+        let from = words[3].parse::<usize>().unwrap_or(0);
+        let to = words[5].parse::<usize>().unwrap_or(0);
+    
+        //println!("move {} from {} to {}", cuantity, from, to);
+
+        let mut aux: VecDeque<char> = VecDeque::new();
+
+        for _ in 0..cuantity {
+            let element = crates[from-1].pop_back().unwrap();
+            aux.push_back(element);
+        }
+
+        for _ in 0..cuantity {
+            let element = aux.pop_back().unwrap();
+            crates[to-1].push_back(element);
+        }
+    }
+    
+    for depot in crates.iter_mut() {
+        let last_element = depot.pop_back().unwrap();
+        print!("{:?}", last_element);
+    }
+
+}
+
 
 
 fn main() {
     let data = read_data("src/data.txt");
-
+    part_2(&data);
 }
