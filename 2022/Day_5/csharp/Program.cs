8bb1736433adcbd07c4de4c5ecca5476d79e9601
Adrian <arrodriguez1809@gmail.com>
2022-12-05 18:21:00 +0100

    2022 Day_5 creation

---
+++ 2022/Day_5/csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Day_5 day = new();
+
+        System.Console.WriteLine(day.Answer1());
+    }
+}
+
+public class Day_5
+{
+    string input;
+
+    public Day_5()
+    {
+        input = File.ReadAllText("../AOCinput");
+    }
+
+    public string Answer1()
+    {
+        return "";
+    }
+
+    public string Answer2()
+    {
+        return "";
+    }
+}
