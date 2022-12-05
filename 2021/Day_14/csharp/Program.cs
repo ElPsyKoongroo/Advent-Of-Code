868b53f299369a1300b3817e814ed4edfa369096
Adrian <arrodriguez1809@gmail.com>
2022-12-05 19:01:06 +0100

    GetInput script created

---
+++ 2021/Day_14/csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfcode;
+public static class Program
+{
+    public static void Main()
+    {
+        Day_14 day = new();
+        day.Answer1();
+    }
+}
+
+
+public class Day_14
+{
+    string input;
+    public Day_14()
+    {
+        input = File.ReadAllText("../AOCinput");
+    }
+
+    public void Answer1()
+    {
+        
+    }
+
+    public void Answer2()
+    {
+        
+    }
+}
---
+++ GetInputCS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+if (!File.Exists("cookies"))
+{
+    Console.WriteLine("Hace falta un archivo \"cookies\"");
+    Environment.Exit(1);
+} 
+
+HttpClient client = new();
+
+HttpRequestHeader reqHed = HttpRequestHeader.Cookie;
+
+HttpRequestMessage req = new();
+
+
+client.DefaultRequestHeaders.
