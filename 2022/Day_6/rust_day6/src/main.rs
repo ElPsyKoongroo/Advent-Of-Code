6fce5b42abb9099d658ab26599d8d845bad36e19
sergious234 <sergious234@gmail.com>
2022-12-06 13:14:35 +0100

    2022 Day6 Full Rust

---
+++ 2022/Day_6/rust_day6/src/main.rs
@@ -0,0 +1,45 @@
+use std::{io::Read, collections::VecDeque};
+use regex::Regex;
+
+fn read_data(file_name: &str) -> String {
+    let mut reader = std::io::BufReader::new(std::fs::File::open(file_name).unwrap());
+    let mut data = String::new();
+    reader.read_to_string(&mut data).unwrap();
+    data
+}
+
+fn part_1(data: &str){
+    let mut readed: VecDeque<char> = VecDeque::new();
+    let mut count = 0;
+    for c in data.chars() {
+        count += 1;
+        if !readed.contains(&c){readed.push_back(c);}
+        else {
+            while readed.pop_front().unwrap() != c {}
+            readed.push_back(c);
+        }
+        if readed.len() == 4 {break}
+    }
+
+    println!("{}, {:?}", count, readed);
+}
+
+fn part_2(data: &str){
+    let mut readed: VecDeque<char> = VecDeque::new();
+    let mut count = 0;
+    for c in data.chars() {
+        count += 1;
+        if !readed.contains(&c){readed.push_back(c);}
+        else {
+            while readed.pop_front().unwrap() != c {}
+            readed.push_back(c);
+        }
+        if readed.len() == 14 {break}
+    }
+
+    println!("{}, {:?}", count, readed);
+}
+fn main() {
+    let data = read_data("src/data.txt");
+    part_2(&data);
+}
