81884bc474b502137e26a0f00cc988664c26b326
Adrian <arrodriguez1809@gmail.com>
2023-01-23 03:14:42 +0100

    2022 Day_16 c_sharp init

---
+++ 2022/Day_16/c_sharp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Day_16 day = new();
+        day.Answer1();
+    }
+}
+
+public class Day_16
+{
+    string[] testLines;
+    string[] inputLines;
+    public Day_16()
+    {
+        testLines = File.ReadAllLines("../AOCtest");
+        inputLines = File.ReadAllLines("../AOCinput");
+    }
+
+    public void Answer1()
+    {
+
+    }
+    public void Answer2()
+    {
+        
+    }
+}
