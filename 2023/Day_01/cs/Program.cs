d4dac04429ae5ad49a7505e60fd5bb2182e270f8
Adrian <arrodriguez1809@gmail.com>
2023-12-03 20:08:54 +0100

    2023 Day01 cs

---
+++ 2023/Day_01/cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace cs;
+
+class Program
+{
+    static Regex r = new("(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)", RegexOptions.Compiled);
+    static void Main(string[] args)
+    {
+        StringBuilder inp = new();
+        string? linea;
+        while ((linea = Console.ReadLine()) is not null)
+        {
+            inp.AppendLine(linea);
+        }
+        string input = inp.ToString()[..^1];
+        Console.WriteLine(Sol1(input));
+        Console.WriteLine(Sol2(input));
+    }
+
+    static string[] nums = 
+    [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    ];
+
+    static int Sol1(string input) => 
+        input.Split("\n").Select(l => {
+            var arr = l.Where(char.IsAsciiDigit).ToArray();
+            return int.Parse($"{arr.First()}{arr.Last()}");
+        }).Sum();
+
+    static int Sol2(string input) =>
+        input.Split("\n").Select(l => {
+            Match matchObj = r.Match(l);
+            var f = matchObj.Value;
+            string e = f;
+            while (matchObj.Success) {
+                e = matchObj.Value;
+                matchObj = r.Match(l, matchObj.Index + 1); 
+            }
+            return int.Parse($"{nums.AsSpan().IndexOf(f)%10}{nums.AsSpan().IndexOf(e)%10}");
+        }).Sum();
+}
