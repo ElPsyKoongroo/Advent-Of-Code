8ca37a0c1f6e832160c00211cf596ec321c8c304
Eduard4033 <eduardneacsa100@gmail.com>
2023-12-03 17:12:25 +0100

    cmusmus day 3

--- 2023/Day_03/CMusMus/main.cpp
+++ 2023/Day_03/CMusMus/main.cpp
@@ -15,9 +15,6 @@ namespace rv = std::ranges::views;
 
 std::string path = "input.txt";
 
-auto isNum = [](char it) { return std::isdigit(it); };
-auto isAlpha = [](char it) { return std::isalpha(it); };
-
 std::vector<std::string> split(std::string input, std::string delimiter = "\n"){
     std::vector<std::string> lines;
     size_t pos = 0;
@@ -42,44 +39,113 @@ std::vector<std::string> read(std::string path){
     delete buffer;
     return split(strbuff);
 }
-bool isInteger(const std::string& str) {
-    try {
-        std::stoi(str);
-        return true;
-    } catch (std::exception e) {
-        return false;
-    }
-}
 
 struct Number{
     std::string value;
     int x,y;
+    std::string xLocation;
 };
 
-int one(std::vector<std::string> input){
-    std::vector<Number> numbers;
-    for( auto [idx, line] : rv::enumerate(input) ){
-        int pos = 0;
-        std::stringstream iss(line);
-        std::string token;
-        while (std::getline(iss, token, '.')) {
-            token.erase(std::remove_if(token.begin(), token.end(),
-                        [](unsigned char x) { return !std::isdigit(x); }), token.end());
-            if (!token.empty()) {
-                numbers.push_back(Number{ token, (int)idx, pos} );
+bool checkSymbols(Number& n, std::vector<std::string>& input){
+    for( int layer = -1; layer<2 ; ++layer){
+        int x = n.x + layer;
+        if(x < 0 || x >= input.size()) continue;
+        int size = (n.value.size()+1) ;
+        for(int pos = -1; pos < size ; ++pos ){
+            int y = n.y + pos;
+            int lsize = input[y].size();
+            if(y < 0 || y >= lsize) continue;
+            char token = input[x][y];
+            if(!std::isdigit(token) && (token != '.')){
+                return true;
             }
-            pos += token.size() + 1;
         }
-    }
-    return 0;
+    }  
+    return false;
 }
 
+bool checkX(Number& n, std::vector<std::string>& input){
+    for( int layer = -1; layer<2 ; ++layer){
+        int x = n.x + layer;
+        if(x < 0 || x >= input.size()) continue;
+        int size = (n.value.size()+1) ;
+        for(int pos = -1; pos < size ; ++pos ){
+            int y = n.y + pos;
+            int lsize = input[y].size();
+            if(y < 0 || y >= lsize) continue;
+            char token = input[x][y];
+            if((token == '*')){
+                n.xLocation += std::to_string(x); 
+                n.xLocation += std::to_string(y) ; 
+                return true;
+            }
+        }
+    }  
+    return false;
+}
+
+int one(std::vector<std::string>& input){
+    std::vector<Number> numbers;
+    int count = 0;
+    std::string currentNumber = "";
+    for( auto [idx, line] : rv::enumerate(input) ){
+        for (int i = 0; i < line.size(); ++i) {
+            if (std::isdigit(line[i])) {
+                currentNumber += line[i];
+            } else if (!currentNumber.empty()) {
+                numbers.emplace_back(Number{currentNumber, (int)idx, (int)(i - currentNumber.size())});
+                currentNumber.clear();
+            }
+        }
+        if (!currentNumber.empty()) {
+            numbers.emplace_back(Number{currentNumber, (int)idx, (int)(line.size() - currentNumber.size())});
+            currentNumber.clear();
+        }
+    }
+   for(auto& num : numbers){
+        if(checkSymbols(num,input)){
+             count += std::stoi(num.value);
+        }
+    }
+    return count;
+}
+
+
 int two(std::vector<std::string> input){
-    return 0;
+    std::unordered_map<std::string, std::vector<Number>> closeToX;
+    std::vector<Number> numbers;
+    int count = 0;
+    std::string currentNumber = "";
+    for( auto [idx, line] : rv::enumerate(input) ){
+        for (int i = 0; i < line.size(); ++i) {
+            if (std::isdigit(line[i])) {
+                currentNumber += line[i];
+            } else if (!currentNumber.empty()) {
+                numbers.emplace_back(Number{currentNumber, (int)idx, (int)(i - currentNumber.size())});
+                currentNumber.clear();
+            }
+        }
+        if (!currentNumber.empty()) {
+            numbers.emplace_back(Number{currentNumber, (int)idx, (int)(line.size() - currentNumber.size())});
+            currentNumber.clear();
+        }
+    }
+    for(auto& num : numbers){
+        if(checkX(num,input)){
+            closeToX[num.xLocation].push_back(num);
+        }
+    }
+
+    for(auto [value, vecs] : closeToX ){
+        if(vecs.size() == 2){
+            count += (std::stoi(vecs[0].value) * std::stoi(vecs[1].value));
+        }
+    }
+    return count;
 }
 
 int main(){
     std::vector<std::string> v = read(path);
     std::cout << one(v) << "\n";
-    //std::cout << two(v) << "\n";
+    std::cout << two(v) << "\n";
 }
