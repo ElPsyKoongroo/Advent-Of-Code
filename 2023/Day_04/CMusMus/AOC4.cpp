42bfb47e38f15438266b29a04b28054a2879b2e8
Eduard4033 <76565274+Eduard4033@users.noreply.github.com>
2023-12-04 12:59:32 +0100

    Add files via upload

---
+++ 2023/Day_04/CMusMus/AOC4.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <utility>
+#include <set>
+#include <ranges>
+namespace rv = std::ranges::views;
+
+std::string path = "input.txt";
+
+std::vector<std::string> split(std::string input, std::string delimiter = "\n") {
+    std::vector<std::string> lines;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = input.find(delimiter)) != std::string::npos) {
+        token = input.substr(0, pos);
+        lines.push_back(token);
+        input.erase(0, pos + delimiter.length());
+    }
+    lines.push_back(input);
+    return lines;
+}
+
+std::vector<std::string> read(std::string path) {
+    std::fstream f(path, std::ios::in);
+    f.seekg(0, f.end);
+    int fileSize = f.tellg();
+    f.seekg(0, f.beg);
+    char* buffer = new char[fileSize];
+    f.read(buffer, fileSize);
+    std::string strbuff = buffer;
+    delete[] buffer;
+    return split(strbuff);
+}
+
+bool isNum(std::string in) {
+    try {
+        std::stoi(in);
+        return true;
+    }
+    catch(std::exception e){
+        return false;
+    }
+}
+
+
+int one(std::vector<std::string> input) {
+    int count = 0;
+    for (auto& line : input) {
+        int winNumCount = 0;
+        line = line.substr(line.find(":")+1);
+        std::string token;
+        std::stringstream iss(line);
+        std::set<int> winNums;
+        iss >> token;
+        while (isNum(token)) {
+            winNums.insert(std::stoi(token));
+            token.clear();
+            iss >> token;
+        }
+        token.clear();
+        iss >> token;
+        while (isNum(token)) {
+            auto [it, inserted] = winNums.insert(std::stoi(token));
+            if (!inserted) winNumCount++;
+            token.clear();
+            iss >> token;
+        }
+
+        if (winNumCount == 0) continue;
+        else count += pow(2, winNumCount - 1);
+    }
+    return count;
+}
+
+int two(std::vector<std::string> input) {
+    std::unordered_map<int, int> boletos;
+    boletos[1] = 1;
+    int size = input.size();
+    while (size--) {
+        boletos[size] = 1;
+    }
+    int count = 0;
+    int idx = 1;
+    for (auto& line : input) {
+        int boletitos = boletos[idx];
+        int winNumCount = 1;
+        line = line.substr(line.find(":") + 1);
+        std::string token;
+        std::stringstream iss(line);
+        std::set<int> winNums;
+        iss >> token;
+        while (isNum(token)) {
+            winNums.insert(std::stoi(token));
+            token.clear();
+            iss >> token;
+        }
+        token.clear();
+        iss >> token;
+        while (isNum(token)) {
+            auto [it, inserted] = winNums.insert(std::stoi(token));
+            if (!inserted) {
+                boletos[idx + winNumCount++]+= boletitos;
+            }
+            token.clear();
+            iss >> token;
+        }
+        
+        idx++;
+    }
+
+    for (auto [boleto, cantidad] : boletos) {
+        count += cantidad;
+    }
+    return count;
+}
+
+int main()
+{
+    auto v = read(path);
+    std::cout << one(v) << "\n";
+    std::cout << two(v);
+}
