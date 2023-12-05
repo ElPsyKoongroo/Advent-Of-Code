8c06e6f9e9147715a03e8dd14b7a39e5284a6ab4
Adrian <arrodriguez1809@gmail.com>
2023-12-05 12:34:42 +0100

    2023 Day05 CMusMus

---
+++ 2023/Day_05/CMusMus/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <utility>
+#include <set>
+#include <ranges>
+#include <map>
+namespace rv = std::ranges::views;
+
+using u64 = u_int64_t;
+
+struct Map {
+    u64 dest,source,length;
+};
+std::string path = "input.txt";
+std::vector<u64> seeds;
+std::vector<std::vector<Map>> sections;
+
+std::vector<std::string> split(std::string input, std::string delimiter = "\n") {
+    std::vector<std::string> lines;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = input.find(delimiter)) != std::string::npos) {
+        token = input.substr(0, pos);
+        lines.push_back(token);
+        input.erase(0, pos + delimiter.length());
+    }
+    lines.push_back(input);
+    return lines;
+}
+
+std::vector<std::string> read(std::string path) {
+    std::fstream f(path, std::ios::in);
+    f.seekg(0, f.end);
+    int fileSize = f.tellg();
+    f.seekg(0, f.beg);
+    char* buffer = new char[fileSize];
+    f.read(buffer, fileSize);
+    std::string strbuff = buffer;
+    delete[] buffer;
+    return split(strbuff, "\n\n");
+}
+
+bool isNum(std::string in) {
+    try {
+        std::stoull(in);
+        return true;
+    }
+    catch(std::exception e){
+        return false;
+    }
+}
+auto toU64 = [](std::string num){ return std::stoull(num) ;};
+void parse(std::vector<std::string> input){
+    auto listaSeeds = split(input[0].substr(input[0].find(":")));
+    std::ranges::copy(listaSeeds | rv::transform(toU64) , std::back_inserter(seeds) );
+
+    for(auto& in : input | rv::drop(1)){
+        sections.push_back(std::vector<Map>());
+        auto nums = split(in);
+        for(auto& num : nums | rv::drop(1)){
+            auto mapita = Map();
+            std::stringstream iss(num);
+            std::string token;
+            iss >> token;
+            mapita.dest = toU64(token);
+            token.clear();
+            iss >> token;
+            mapita.source = toU64(token);
+            token.clear();
+            iss >> token;
+            mapita.length = toU64(token);
+            sections.back().push_back(mapita);
+        }
+    }
+}
+
+int one(std::vector<std::string> input) {
+
+}
+
+int two(std::vector<std::string> input) {
+
+}
+
+int main()
+{
+    auto v = read(path);
+    parse(v);
+    std::cout << one(v) << "\n";
+    std::cout << two(v) << "\n";
+}
