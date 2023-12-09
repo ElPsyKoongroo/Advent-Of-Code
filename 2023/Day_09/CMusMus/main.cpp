34fb52df70b3d8c04190b32f2c52db38289c95ab
Eduard4033 <eduardneacsa100@gmail.com>
2023-12-09 15:02:13 +0100

    day9 completed

--- 2023/Day_09/CMusMus/main.cpp
+++ 2023/Day_09/CMusMus/main.cpp
@@ -19,6 +19,18 @@ namespace rv = std::ranges::views;
 
 constexpr std::string path = "input.txt";
 
+std::vector<std::vector<int>> histories;
+
+bool isNum(std::string str) {
+    try {
+        std::stoi(str);
+        return true;
+    }
+    catch (std::exception e) {
+        return false;
+    }
+}
+
 std::vector<std::string> split(std::string input, std::string delimiter = "\n") {
     std::vector<std::string> lines;
     size_t pos = 0;
@@ -45,14 +57,61 @@ std::vector<std::string> read(std::string path) {
 }
 
 void parse(std::vector<std::string> input) {
+    for(auto& line : input){
+        std::string token;
+        histories.push_back(std::vector<int>());
+        std::stringstream iss(line);
+        iss >> token;
+        while(isNum(token)){
+            histories.back().push_back(std::stoi(token));
+            token.clear();
+            iss >> token;
+        }
+    }
 }
 
-int one() {
-    return 0;
+auto Zero = [](const int64_t i){ return i == 0; };
+auto adjDiff = [](int64_t a, int64_t b){ return b - a ;};
+auto lastElem = [](std::vector<int64_t> vec){ return vec.back(); };
+auto firstElem = [](std::tuple<std::ptrdiff_t, std::vector<int64_t> &> idx_vec) { 
+    return std::get<0>(idx_vec) % 2 ? - (std::get<1>(idx_vec).front()) : std::get<1>(idx_vec).front() ;
+};
+
+int64_t one() {
+    int64_t count = 0;
+    for( auto& history : histories ){
+        std::vector<std::vector<int64_t>> topDown;
+        topDown.push_back(std::vector<int64_t>(history.begin(),history.end()));
+        while( !std::ranges::all_of(topDown.back() , Zero ) ){
+            auto diffs = topDown.back() | rv::adjacent_transform<2>(adjDiff );    
+            topDown.push_back(std::vector<int64_t>(diffs.begin() , diffs.end()));
+        }
+        auto last = topDown | rv::transform(lastElem) ;
+        count += std::accumulate(last.begin(), last.end(), 0);
+    }
+    return count;
 }
 
-uint64_t two() {
-    return 0;
+int64_t two() {
+    int64_t count = 0;
+    for( auto& history : histories ){
+        std::vector<std::vector<int64_t>> topDown;
+        topDown.push_back(std::vector<int64_t>(history.begin(),history.end()));
+        while( !std::ranges::all_of(topDown.back() , Zero ) ){
+            auto diffs = topDown.back() | rv::adjacent_transform<2>(adjDiff );    
+            topDown.push_back(std::vector<int64_t>(diffs.begin() , diffs.end()));
+        }
+        
+        for( auto [idx, vec] : topDown | rv::enumerate | rv::reverse | rv::drop(1) ){
+            vec.insert(vec.begin(), - topDown[idx+1].front() + vec.front());
+        }
+
+        count += topDown.front().front();
+        // auto first = topDown | rv::reverse | rv::enumerate | rv::transform(firstElem) ;
+        // std::vector<int64_t> firsts (first.begin(), first.end());
+        // count += std::accumulate(first.begin(), first.end(), 0);
+    }
+    return count;
 }
 
 int main()
