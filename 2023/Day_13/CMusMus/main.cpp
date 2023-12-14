da0560a88378aafa3f15f9a28fef064dcd177149
Adrian <arrodriguez1809@gmail.com>
2023-12-14 15:41:02 +0100

    2023 Day13 cmm

---
+++ 2023/Day_13/CMusMus/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <utility>
+#include <set>
+#include <ranges>
+#include <map>
+#include <chrono>
+
+namespace rv = std::ranges::views;
+std::string path = "input.txt";
+
+std::vector<std::string> split(std::string input, std::string delimiter = "\n") {
+    std::vector<std::string> lines;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = input.find(delimiter)) != std::string::npos) {
+        token = input.substr(0, pos);
+        lines.push_back(token);
+        input.erase(0, pos + delimiter.length());
+    }
+    lines.push_back(input);
+    return lines;
+}
+
+std::vector<std::string> read(std::string path) {
+    std::fstream f(path, std::ios::in);
+    f.seekg(0, f.end);
+    int fileSize = f.tellg();
+    f.seekg(0, f.beg);
+    char* buffer = new char[fileSize];
+    f.read(buffer, fileSize);
+    std::string strbuff = buffer;
+    delete[] buffer;
+    return split(strbuff , "\n\n");
+}
+
+uint64_t one(std::vector<std::vector<std::string>>& input) {
+    uint64_t count = 0;
+    for (auto& group : input){
+        for( auto [ idx, line] : group | rv::enumerate | rv::drop(1) ){
+            auto r = line | rv::split(idx);
+            std::vector<std::string> parts(line.begin(), line.end());
+            auto pleg = rv::zip(parts[0], parts[1] | rv::reverse );
+            int times = std::ranges::count_if( pleg , [](std::pair<char,char> p){ return p.first == p.second; });
+            //if( times != std::ranges::min(parts[0].size(), parts[1].size()) ) ;
+        }
+    }
+    return count;
+}
+
+uint64_t two(std::vector<std::vector<std::string>>& input) {
+    uint64_t count = 0;
+
+    return count;
+}
+
+int main()
+{
+    auto data = read(path);
+    auto v = rv::transform ( data, [](std::string str) { return split(str); } );
+    std::vector<std::vector<std::string>> arr(v.begin(), v.end());
+    std::cout << one(arr) << "\n";
+    std::cout << two(arr) << "\n";
+    return 0;
+}
