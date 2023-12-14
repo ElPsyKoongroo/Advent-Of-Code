15304667e02d49f31ffa193fb40b0eca6aaca51d
Adrian <arrodriguez1809@gmail.com>
2023-12-14 14:32:49 +0100

    2023 Day14 cs complete

--- 2023/Day_14/cs/Program.cs
+++ 2023/Day_14/cs/Program.cs
@@ -39,8 +39,8 @@ class Program
     static void Main(string[] args)
     {
         string path = args.FirstOrDefault("../AOCinput");
-        System.Console.WriteLine(Sol1(path));
-        // System.Console.WriteLine(Sol2(path));
+        // System.Console.WriteLine(Sol1(path));
+        System.Console.WriteLine(Sol2(path));
     }
 
     static char[][] ParseInput(string path) =>
@@ -80,7 +80,108 @@ class Program
         return total;
     }
     static int Sol2(string path) {
-        var input = ParseInput(path);
-        return 0;
+        var actualCycle = ParseInput(path);
+
+        List<char[][]> cycles = [];
+
+        TiltNorth(actualCycle);
+        TiltWest(actualCycle);
+        TiltSourth(actualCycle);
+        TiltEast(actualCycle);
+
+        while(!cycles.Any(c=> PlatformEqual(c, actualCycle))) {
+            cycles.Add(actualCycle.Select(x=> x.ToArray()).ToArray());
+
+            TiltNorth(actualCycle);
+            TiltWest(actualCycle);
+            TiltSourth(actualCycle);
+            TiltEast(actualCycle);
+        }
+
+        int untilCycle = 0, sizeCycle = 0;
+        for(int i = 0; i < cycles.Count - 1; ++i) {
+            if(PlatformEqual(cycles[i], actualCycle)) {
+                untilCycle = i+1;
+                sizeCycle = cycles.Count - i;
+                break;
+            }
+        }
+        int totalCycles = 1_000_000_000;
+        int finalCycleIndex = ((totalCycles - untilCycle)%sizeCycle)+untilCycle-1;
+        var finalCycle = cycles[finalCycleIndex];
+
+        int total = 0;
+        var totalRows = finalCycle.Length;
+        for(int i = 0; i < totalRows; ++i) {
+            total += finalCycle[i].Count(x=> x=='O') * (totalRows-i);
+        }
+
+        return total;
     }
+
+    static bool PlatformEqual(char[][] p1, char[][] p2) {
+        for(int i = 0; i < p1.Length; ++i) {
+            for(int j = 0; j < p1[0].Length; ++j) {
+                if(p1[i][j] != p2[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+    static void TiltNorth(char[][] input) {
+        for(int i = 1; i < input.Length; ++i) {
+            for(int j = 0; j < input[0].Length; ++j) {
+                if(input[i][j] != 'O') continue;
+                int actualI = i;
+                while(actualI-1 >= 0 && input[actualI-1][j] == '.') {
+                    input[actualI-1][j] = 'O';
+                    input[actualI][j] = '.';
+                    actualI--;
+                }
+            }
+        }
+    }
+
+    static void TiltSourth(char[][] input) {
+        for(int i = input.Length-2; i >= 0; --i) {
+            for(int j = 0; j < input[0].Length; ++j) {
+                if(input[i][j] != 'O') continue;
+                int actualI = i;
+                while(actualI+1 < input.Length && input[actualI+1][j] == '.') {
+                    input[actualI+1][j] = 'O';
+                    input[actualI][j] = '.';
+                    actualI++;
+                }
+            }
+        }
+    }
+
+    static void TiltWest(char[][] input) {
+        for(int j = 1; j < input[0].Length; ++j) {
+            for(int i = 0; i < input.Length; ++i) {
+                if(input[i][j] != 'O') continue;
+                int actualJ = j;
+                while(actualJ-1 >= 0 && input[i][actualJ-1] == '.') {
+                    input[i][actualJ-1] = 'O';
+                    input[i][actualJ] = '.';
+                    actualJ--;
+                }
+            }
+        }
+    }
+
+    static void TiltEast(char[][] input) {
+        for(int j = input[0].Length-2; j >= 0; --j) {
+            for(int i = 0; i < input.Length; ++i) {
+                if(input[i][j] != 'O') continue;
+                int actualJ = j;
+                while(actualJ+1 < input[0].Length && input[i][actualJ+1] == '.') {
+                    input[i][actualJ+1] = 'O';
+                    input[i][actualJ] = '.';
+                    actualJ++;
+                }
+            }
+        }
+    }
+
 }
