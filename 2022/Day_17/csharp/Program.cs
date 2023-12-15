f724e812830b4b882d1466faa9f7748f6ddaa897
Adrian <arrodriguez1809@gmail.com>
2023-12-15 22:58:00 +0100

    2022 Day17 cs complete

--- 2022/Day_17/csharp/Program.cs
+++ 2022/Day_17/csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Xml;
 
 namespace AdventOfCode;
 public class Program
@@ -105,7 +106,6 @@ public class Day_17
         int Y = 3;
         int X = 2;
 
-        int pipo = 0;
 
         KeyValuePair<int, int>[] actualPiece = BaseRocks[actualPiecePos].Select(
                 block => new KeyValuePair<int, int>(block.Key + X, (int)block.Value + Y)).ToArray();
@@ -171,19 +171,57 @@ public class Day_17
         return (maxHeight+1).ToString();
     }
 
-    private static void PrintState(HashSet<KeyValuePair<int, int>>[] towers, int height)
+    private static void PrintState(HashSet<(int, int)>[] towers, int height)
     {
         for (int i = height; i >= 0; --i)
         {
             for(int j = 0; j < 7; ++j)
             {
-                System.Console.Write((towers[j].Contains(new KeyValuePair<int, int>(j, i)) ? "#" : "."));
+                System.Console.Write((towers[j].Contains((j, i)) ? "#" : "."));
             }
             System.Console.WriteLine();
         }
         System.Console.WriteLine();
         System.Console.WriteLine();
     }
+
+    private static (int, int)[] SaveState(HashSet<(int X, int Y)>[] towers, int saveSize, int height) {
+        return 
+            towers
+            .SelectMany(t => 
+                t.Where(x => 
+                    x.Y > height-saveSize
+                )
+                .Select(x=> (x.X, x.Y-(height-saveSize)))
+            ).ToArray();
+    }
+    
+    private static bool CheckAlreadySeen(
+        (int X, int Y)[] actual, 
+        List<((int X, int Y)[], int wind, int rock, int Y, long totalRocksFallen)> seen, 
+        int actualWindPos, int actualPiecePos,
+        out int last)
+    {
+        last = -1;
+        foreach(var ((towers, wind, rock, _, _), i) in seen.Zip(Enumerable.Range(0, seen.Count))) {
+            if(towers.Length != actual.Length) continue;
+            if(actualWindPos != wind) continue;
+            if (actualPiecePos != rock) continue;
+
+            bool ok = true;
+            foreach(var pos in towers) {
+                if(!actual.Contains(pos))  {
+                    ok = false;
+                    break; 
+                }
+            }
+            if(!ok) continue;
+            last = i;
+            return true;
+        }
+        return false;
+    }
+
 
     private static string Answer2(bool test)
     {
@@ -192,30 +230,58 @@ public class Day_17
         const int width = 7;
         const int totalPieces = 5;
 
-        var towers = Enumerable.Range(0, width).Select(_ => new HashSet<long>()).ToArray();
+        var towers = Enumerable.Range(0, width).Select(_ => new HashSet<(int X, int Y)>()).ToArray();
 
-        long maxHeight = -1;
+        int maxHeight = -1;
         int totalWinds = input.Length;
         int actualPiecePos = 0;
         int actualWindPos = 0;
 
         long totalRocksFallen = 0;
+        int saveSize = 30;
 
-        long Y = 3;
+        long totalIters = 1_000_000_000_000;
+
+        int Y = 3;
         int X = 2;
+        bool skiped = false;
+        long add = 0;
+        List<((int x, int y)[], int wind, int rock, int y, long totalRocks)> seen = [];
 
-        KeyValuePair<int, long>[] actualPiece = BaseRocks[actualPiecePos];
+
+        (int X, int Y)[] actualPiece = BaseRocks[actualPiecePos].Select(
+                block => (block.Key + X, (int)block.Value + Y)).ToArray();
 
         while(true)
         {
+            if(totalRocksFallen == totalIters) break;
+            if(!skiped && maxHeight > saveSize) {
+                var actual = SaveState(towers, saveSize, maxHeight);
+
+                if(CheckAlreadySeen(actual, seen, actualWindPos, actualPiecePos, out int last) 
+                    /*&& totalRocksFallen > 2022*/) {
+                    skiped = true;
+                    // last++;
+                    var dy = (Y-4) - seen[last].y;
+                    var dr = totalRocksFallen - seen[last].totalRocks;
+                    long amt = (totalIters-totalRocksFallen)/dr;
+                    add = amt*dy;
+                    totalRocksFallen += amt*dr;
+                    // actualPiecePos = (int)(totalRocksFallen%5);
+                    continue;
+                }
+                seen.Add((actual, actualWindPos, actualPiecePos, maxHeight, totalRocksFallen));
+            }
             int actualWindDir = input[actualWindPos] == '>' ? 1 : -1;
             actualWindPos = (actualWindPos+1) % totalWinds;
             bool hit = false;
 
-            foreach(var block in actualPiece)
+            (int Key, int Value)[] movedRock = actualPiece.Select(
+                block => (block.X + actualWindDir, block.Y)).ToArray();
+
+            foreach(var block in movedRock)
             {
-                int xValue = block.Key+X+actualWindDir;
-                if(xValue < 0 || xValue >= width || towers[xValue].Contains(block.Value+Y))
+                if(block.Key < 0 || block.Key >= width || towers[block.Key].Contains(block))
                 {
                     hit = true;
                     break;
@@ -224,15 +290,16 @@ public class Day_17
             if(!hit)
             {
                 X += actualWindDir;
+                actualPiece = movedRock;
             }
 
             hit = false;
+            movedRock = actualPiece.Select(
+                block => (block.X, block.Y - 1)).ToArray();
 
-            foreach(var block in actualPiece)
+            foreach(var block in movedRock)
             {
-                int xValue = block.Key+X;
-                long yValue = block.Value+Y-1;
-                if(yValue < 0 || towers[xValue].Contains(yValue))
+                if(block.Value < 0 || towers[block.Key].Contains(block))
                 {
                     hit = true;
                     break;
@@ -240,28 +307,31 @@ public class Day_17
             }
             if(!hit)
             {
-                Y--;
+                actualPiece = movedRock;
+                continue;
             }
-            else
+            foreach(var block in actualPiece)
             {
-                foreach(var block in actualPiece)
-                {
-                    int xValue = block.Key+X;
-                    long yValue = block.Value + Y;
-                    if(yValue > maxHeight) maxHeight = yValue;
-                    towers[xValue].Add(yValue);
-                }
-                Y = maxHeight + 4;
-                X = 2;
-
-                actualPiecePos = (actualPiecePos+1) % totalPieces;
-                actualPiece = BaseRocks[actualPiecePos];
-
-                totalRocksFallen++;
-                if(totalRocksFallen == 1000000000000) break;
+                if(block.Y > maxHeight) maxHeight = block.Y;
+                towers[block.X].Add(block);
             }
+            Y = maxHeight + 4;
+            X = 2;
+
+            actualPiecePos = (actualPiecePos+1) % totalPieces;
+            actualPiece = BaseRocks[actualPiecePos].Select(
+                block => (block.Key + X, (int)block.Value + Y)).ToArray();
+
+            totalRocksFallen++;
         } 
 
-        return (maxHeight+1).ToString();
+        // System.Console.WriteLine(
+        //     "Se fini"
+        // );
+
+        // PrintState(towers, maxHeight);
+        // System.Console.WriteLine(add);
+        return (maxHeight+1+add).ToString();
     }
+
 }
