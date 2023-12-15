480af17a28a7aed62e7ffeb569eafa18484467f8
Adrian Rodriguez <arrodriguez1809@gmail.com>
2023-12-15 11:11:12 +0100

    2023 Day_15 cs start

---
+++ 2023/Day_15/cs/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace cs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = args.FirstOrDefault("../AOctest");
+        Console.WriteLine(Sol1(path));
+        Console.WriteLine(Sol2(path));
+    }
+
+    static int Sol1(string path) {
+        
+    }
+}
