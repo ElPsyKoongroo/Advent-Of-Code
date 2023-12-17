f14f99f0f4b7e9a06640a2f4e1a1c4a145c8f1ff
Adrian <arrodriguez1809@gmail.com>
2023-12-17 02:23:51 +0100

    2022 Day18 cs complete

--- 2022/Day_18/cs/Program.cs
+++ 2022/Day_18/cs/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Security.Cryptography;
-
-namespace cs;
-
+﻿namespace cs;
 
 class Program
 {
@@ -43,28 +40,69 @@ class Program
         int minY = input.MinBy(X=>X.y).y;
         int minZ = input.MinBy(X=>X.z).z;
 
-        // Console.WriteLine($"X {minX} - {maxX}");
-        // Console.WriteLine($"Y {minY} - {maxY}");
-        // Console.WriteLine($"Z {minZ} - {maxZ}");
+        List<(int x, int y, int z)> noInfo = [];
 
-        HashSet<(int x, int y, int z)> noInfo = [];
-
-        for(int x = minX; x <= maxX; ++x) {
-            for(int y = minY; y <= maxY; ++y) {
-                for(int z = minZ; z <= maxZ; ++z) {
+        for(int x = minX-1; x <= maxX+1; ++x) {
+            for(int y = minY-1; y <= maxY+1; ++y) {
+                for(int z = minZ-1; z <= maxZ+1; ++z) {
                     var p = (x,y,z);
                     if(!input.Contains(p)) noInfo.Add(p);
                 }
             }
         }
 
-        HashSet<(int x, int y, int z)> inside = [];
-        HashSet<(int x, int y, int z)> outside = [];
         Queue<(int x, int y, int z)> actualQueue = [];
         List<(int x, int y, int z)> actualPath = [];
+        List<(int x, int y, int z)> inside = [];
 
-        
 
-        return 1;
+        while(noInfo.Count!=0) {
+            actualQueue.Enqueue(noInfo[0]);
+            noInfo.RemoveAt(0);
+
+            (int x,int y,int z)[] directions = [
+                (1,0,0),(-1,0,0),
+                (0,1,0),(0,-1,0),
+                (0,0,1),(0,0,-1),
+            ];
+            bool isInside = true;
+
+            while(actualQueue.TryDequeue(out var actual)) {
+                actualPath.Add(actual);
+
+                foreach(var (x, y, z) in directions) {
+                    int newX = actual.x+x;
+                    int newY = actual.y+y;
+                    int newZ = actual.z+z;
+                    if(newX > maxX || newX < 0 ||
+                        newY > maxY || newY < 0 ||
+                        newZ > maxZ || newZ < 0) {
+                            isInside = false;
+                            continue;
+                    }
+                    var newP = (newX, newY, newZ);
+                    if(noInfo.Contains(newP)) {
+                        noInfo.Remove(newP);
+                        actualQueue.Enqueue(newP);
+                    }
+                }
+            }
+
+            if(isInside) {
+                inside = [..inside, ..actualPath];
+            }
+            actualPath.Clear();
+        }
+
+        var total = 0;
+        foreach(var (x,y,z) in input) {
+            if(!input.Contains((x+1,y,z)) && !inside.Contains((x+1,y,z))) total++;
+            if(!input.Contains((x-1,y,z)) && !inside.Contains((x-1,y,z))) total++;
+            if(!input.Contains((x,y+1,z)) && !inside.Contains((x,y+1,z))) total++;
+            if(!input.Contains((x,y-1,z)) && !inside.Contains((x,y-1,z))) total++;
+            if(!input.Contains((x,y,z+1)) && !inside.Contains((x,y,z+1))) total++;
+            if(!input.Contains((x,y,z-1)) && !inside.Contains((x,y,z-1))) total++;
+        }
+        return total;
     }
 }
