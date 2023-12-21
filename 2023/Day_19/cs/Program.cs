2b22311a59b068aa19b89a132a8347cc80ea5998
Adrian <arrodriguez1809@gmail.com>
2023-12-22 00:48:57 +0100

    2023 Day19 cs complete

--- 2023/Day_19/cs/Program.cs
+++ 2023/Day_19/cs/Program.cs
@@ -1,13 +1,15 @@
-﻿
-using System.Net.Http.Headers;
-using System.Security.Cryptography;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Newtonsoft.Json;
+using RegExtract;
+using MoreLinq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Diagnostics;
 
 namespace cs;
 
 public static class Extensions {
-        public static void Dump<T>(this T obj, bool indent = false)
+    public static void Dump<T>(this T obj, bool indent = false)
     {
         Console.WriteLine(
         JsonConvert.SerializeObject(obj, indent ? Formatting.Indented : Formatting.None,
@@ -16,45 +18,138 @@ public static class Extensions {
     }
 }
 
-record Part(int X, int M, int A, int S) {
-    private static readonly Regex r = new(@"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}");
-    public Part(string line):this(0,0,0,0) {
-        var match = r.Match(line);
-        X = int.Parse(match.Groups[1].Value);
-        M = int.Parse(match.Groups[2].Value);
-        A = int.Parse(match.Groups[3].Value);
-        S = int.Parse(match.Groups[4].Value);
-    }
-};
 
-record Condition(string Var, char Comparer, int Num, string NextName) {
-    public Condition() : this("", '_', 0, "") {
+record Part(int X, int M, int A, int S);
+record Condition(char Var, char Comparer, int Num, string NextName);
+record Workflow(string Name, List<Condition> Conditions, string Final);
 
-    }
-};
-
-record Workflow(string Name, (string, string, int)[] Conditions);
-
+record PartRange((int s, int e) X, (int s, int e) M, (int s, int e) A, (int s, int e) S);
 class Program
 {
     static void Main(string[] args)
     {
         string path = args.FirstOrDefault("../AOCtest");
+
         Console.WriteLine(Sol1(path));
         Console.WriteLine(Sol2(path));
     }
 
     static (Workflow[], Part[]) ParseInput(string path) {
-        return (null, null);
+        var groups = File.ReadAllLines(path).Split(string.IsNullOrEmpty).ToArray();
+
+        var works = groups[0].Extract<Workflow>(@"(\w+){((\w)(.)(\d+):(\w+),)+(\w+)}").ToArray();
+        var parts = groups[1].Extract<Part>(@"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}").ToArray();
+
+        return (works, parts);
     }
 
     static long Sol1(string path)
     {
-        return -1;
+        var (worksArr, parts) = ParseInput(path);
+
+        var works = worksArr.ToDictionary(x=> x.Name);
+
+        long total = 0;
+        foreach(var part in parts) {
+            string actualWork = "in";
+            while(true) {
+                var conds = works[actualWork].Conditions;
+                bool found = false;
+                foreach(var cond in conds) {
+                    var value = cond.Var switch {
+                        'a' => part.A,
+                        'x' => part.X,
+                        'm' => part.M,
+                        _ => part.S
+                    };
+                    found = cond.Comparer == '<' ? value < cond.Num : value > cond.Num;
+                    if(found) {
+                        actualWork = cond.NextName;
+                        break;
+                    }
+                }
+                if(!found) actualWork = works[actualWork].Final;
+
+                if(actualWork == "A") {
+                    var a = part.A + part.X + part.M + part.S;
+                    total += a;
+                    break;
+                }
+                if(actualWork == "R") {
+                    break;
+                }
+            }
+        }
+
+        return total;
     }
 
     static long Sol2(string path)
     {
-        return -1;
+        var works = ParseInput(path).Item1.ToDictionary(x=> x.Name);
+
+        Queue<(PartRange, string, int)> ranges = [];
+        ranges.Enqueue((new((1,4000),(1,4000),(1,4000),(1,4000)), "in", 0));
+
+        long total = 0;
+        while(ranges.TryDequeue(out var next)) {
+            var (partRanges, work, condition) = next;
+            
+            bool found = false;
+            bool breakCycle = false;
+            for(int i = condition; i < works[work].Conditions.Count; ++i) {
+                var cond = works[work].Conditions[i];
+                var (s, e) = cond.Var switch {
+                    'a' => partRanges.A,
+                    'x' => partRanges.X,
+                    'm' => partRanges.M,
+                    _ => partRanges.S
+                };
+
+                var (rangeEqual, isTrue, offset) = cond.Comparer switch {
+                    '<' => (s < cond.Num == e < cond.Num, e < cond.Num, -1),
+                    _ => (s > cond.Num == e > cond.Num, e > cond.Num, 0)
+                };
+                
+                if(rangeEqual) {
+                    if(isTrue) {
+                        work = cond.NextName;
+                        found = true;
+                        break;
+                    }
+                } else {
+                    var lr = (s, cond.Num+offset);
+                    var ur = (cond.Num+offset+1, e);
+                    
+                    var (newL,newU) = cond.Var switch {
+                        'a' =>  (partRanges with {A=lr}, partRanges with {A=ur}),
+                        'x' =>  (partRanges with {X=lr}, partRanges with {X=ur}),
+                        'm' =>  (partRanges with {M=lr}, partRanges with {M=ur}),
+                        _ =>    (partRanges with {S=lr}, partRanges with {S=ur}),
+                    };
+                    ranges.Enqueue((newL, work, i));
+                    ranges.Enqueue((newU, work, i));
+                    breakCycle = true;
+                    break;
+                }
+            }
+            if(breakCycle) continue;
+            if(!found) {
+                work = works[work].Final;
+            }
+            
+            if(work == "R") continue;
+            if(work == "A")  {
+                long totalx = partRanges.X.e - partRanges.X.s + 1;
+                long totalm = partRanges.M.e - partRanges.M.s + 1;
+                long totala = partRanges.A.e - partRanges.A.s + 1;
+                long totals = partRanges.S.e - partRanges.S.s + 1;
+                total += totalx * totalm * totals * totala;
+                continue;
+            }
+            ranges.Enqueue((partRanges, work, 0));
+        }
+
+        return total;
     }
 }
