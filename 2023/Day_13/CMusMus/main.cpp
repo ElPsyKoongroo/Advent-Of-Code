2830d2fb9ae9f98abbda27ab0510328d9323a102
Adrian <arrodriguez1809@gmail.com>
2023-12-21 18:47:14 +0100

    2023 Day13 cmusmus

--- 2023/Day_13/CMusMus/main.cpp
+++ 2023/Day_13/CMusMus/main.cpp
@@ -45,20 +45,55 @@ std::vector<std::string> read(std::string path) {
 uint64_t one(std::vector<std::vector<std::string>>& input) {
     uint64_t count = 0;
     for (auto& group : input){
-        bool isValid = true;
-        for( auto line : group){
-            for (int i = 1 ; i < line.size() ; ++i ){
-                auto r = line | rv::split(i);
-                std::vector<std::string> parts(line.begin(), line.end());
+        bool isValid = false;
+        // Columnas
+        for (int i = 1 ; i < group.front().size() && !isValid  ; ++i ){
+            isValid = true;
+            for (auto& line : group){
+                auto r = line | rv::chunk(i);
+                std::vector<std::string> parts(r.begin(), r.end());
                 auto pleg = rv::zip(parts[0], parts[1] | rv::reverse );
                 int times = std::ranges::count_if( pleg , [](std::pair<char,char> p){ return p.first == p.second; });
                 if( times != std::ranges::min(parts[0].size(), parts[1].size()) ){
-                    
-                    continue;
+                    isValid = false;
+                    break;
                 }
             }
+            if(isValid){
+                count += i;
+            }
         }
+        if (isValid) continue;
+
+        for (int i = 1 ; i < group.size() && !isValid ; ++i ){
+            isValid = true;
+            for (auto& line : group ){
+                auto r = line | rv::chunk(i);
+                std::vector<std::string> parts(r.begin(), r.end());
+                auto pleg = rv::zip(parts[0], parts[1] | rv::reverse );
+                int times = std::ranges::count_if( pleg , [](std::pair<char,char> p){ return p.first == p.second; });
+                if( times != std::ranges::min(parts[0].size(), parts[1].size()) ){
+                    isValid = false;
+                    break;
+                }
+            }
+            if(isValid){
+                count += i;
+            }
+        }
+
+
+
+
+
     }
+
+
+
+
+
+
+
     return count;
 }
 
