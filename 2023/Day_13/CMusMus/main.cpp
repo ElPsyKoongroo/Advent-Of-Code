e9acc2f14180812e3dfc5c9d98d4ded30f419787
Eduard4033 <eduardneacsa100@gmail.com>
2023-12-24 14:20:17 +0100

    jeje dia 13 CMUSMUS

--- 2023/Day_13/CMusMus/main.cpp
+++ 2023/Day_13/CMusMus/main.cpp
@@ -42,17 +42,25 @@ std::vector<std::string> read(std::string path) {
     return split(strbuff , "\n\n");
 }
 
+auto ith_element = [](int i) {
+    return [i](auto const& v){
+        return v[i];
+    };
+};
+
 uint64_t one(std::vector<std::vector<std::string>>& input) {
     uint64_t count = 0;
     for (auto& group : input){
         bool isValid = false;
-        // Columnas
         for (int i = 1 ; i < group.front().size() && !isValid  ; ++i ){
             isValid = true;
             for (auto& line : group){
+                std::vector<std::string> parts;
                 auto r = line | rv::chunk(i);
-                std::vector<std::string> parts(r.begin(), r.end());
-                auto pleg = rv::zip(parts[0], parts[1] | rv::reverse );
+                for (const auto& chunk : line | rv::chunk(i)) {
+                    parts.push_back(std::string(chunk.begin(), chunk.end()));
+                }
+                auto pleg = rv::zip(parts[0] | rv::reverse , parts[1] );
                 int times = std::ranges::count_if( pleg , [](std::pair<char,char> p){ return p.first == p.second; });
                 if( times != std::ranges::min(parts[0].size(), parts[1].size()) ){
                     isValid = false;
@@ -60,17 +68,29 @@ uint64_t one(std::vector<std::vector<std::string>>& input) {
                 }
             }
             if(isValid){
+                std::cout << "---> "<< i <<" <---\n";
                 count += i;
             }
         }
+
         if (isValid) continue;
 
         for (int i = 1 ; i < group.size() && !isValid ; ++i ){
             isValid = true;
-            for (auto& line : group ){
-                auto r = line | rv::chunk(i);
-                std::vector<std::string> parts(r.begin(), r.end());
-                auto pleg = rv::zip(parts[0], parts[1] | rv::reverse );
+
+            for ( int ic = 0 ; ic < group.front().size() ; ++ic ){
+
+                auto column = [ic](int i) {
+                    return std::views::transform(ith_element(i));
+                };
+                auto l = group | column(ic);
+                std::string line(l.begin(), l.end());
+                
+                std::vector<std::string> parts;
+                for (const auto& chunk : line | rv::chunk(i)) {
+                    parts.push_back(std::string(chunk.begin(), chunk.end()));
+                }
+                auto pleg = rv::zip(parts[0] | rv::reverse, parts[1]);
                 int times = std::ranges::count_if( pleg , [](std::pair<char,char> p){ return p.first == p.second; });
                 if( times != std::ranges::min(parts[0].size(), parts[1].size()) ){
                     isValid = false;
@@ -78,28 +98,92 @@ uint64_t one(std::vector<std::vector<std::string>>& input) {
                 }
             }
             if(isValid){
-                count += i;
+                std::cout << "---7 "<< i <<" L----\n";
+                count += 100*i;
             }
         }
-
-
-
-
-
     }
-
-
-
-
-
-
-
     return count;
 }
 
 uint64_t two(std::vector<std::vector<std::string>>& input) {
     uint64_t count = 0;
-
+        for (auto& group : input){
+        std::cout << "----------------------------------\n";
+        bool isValid = false;
+        bool isSmudge = false;
+        int index = INT32_MAX;
+        for (int i = 1 ; i < group.front().size() ; ++i ){
+            int matches = 0;
+            isValid = true;
+            for (auto& line : group){
+                std::vector<std::string> parts;
+                auto r = line | rv::chunk(i);
+                for (const auto& chunk : line | rv::chunk(i)) {
+                    parts.push_back(std::string(chunk.begin(), chunk.end()));
+                }
+                auto pleg = rv::zip(parts[0] | rv::reverse , parts[1] );
+                int times = std::ranges::count_if( pleg , [](std::pair<char,char> p){ return p.first == p.second; });
+                matches += times;
+                if( times != std::ranges::min(parts[0].size(), parts[1].size()) ){
+                    isValid = false;
+                }
+            }
+            int factor = std::min((int)group.front().size() - i, i);
+            if(matches == group.size() * factor - 1 ){
+                isValid = true;
+                isSmudge = true;
+                index = i;
+                std::cout << "Sm---> "<< i <<" <---\n";
+                break;
+            }
+            if(isValid){
+                index = i;
+                std::cout << "V---> "<< i <<" <---\n";
+                // break;
+            }
+        }
+        if (isSmudge) {count += index ; continue;}
+        isValid = false;
+        for (int i = 1 ; i < group.size(); ++i ){
+            isValid = true;
+            int matches = 0;
+            for ( int ic = 0 ; ic < group.front().size() ; ++ic ){
+                
+                auto column = [ic](int i) {
+                    return std::views::transform(ith_element(i));
+                };
+                auto l = group | column(ic);
+                std::string line(l.begin(), l.end());
+                
+                std::vector<std::string> parts;
+                for (const auto& chunk : line | rv::chunk(i)) {
+                    parts.push_back(std::string(chunk.begin(), chunk.end()));
+                }
+                auto pleg = rv::zip(parts[0] | rv::reverse, parts[1]);
+                int times = std::ranges::count_if( pleg , [](std::pair<char,char> p){ return p.first == p.second; });
+                matches += times;
+                if( times != std::ranges::min(parts[0].size(), parts[1].size()) ){
+                    isValid = false;
+                }
+            }
+            int factor = std::min((int)group.size() - i, i);
+            if(matches == group.front().size() * factor - 1 ){
+                isValid = true;
+                isSmudge = true;
+                index = i;
+                std::cout << "Sm---7 "<< i <<" L----\n";
+                break;
+            }
+            if(isValid){
+                std::cout << "V---7 "<< i <<" L----\n";
+                index = 100 * i;
+                //break;
+            }
+        }
+        if (isSmudge) { count += ( 100 * index ); continue;}
+        else count += ( index );
+    }
     return count;
 }
 
@@ -108,7 +192,7 @@ int main()
     auto data = read(path);
     auto v = rv::transform ( data, [](std::string str) { return split(str); } );
     std::vector<std::vector<std::string>> arr(v.begin(), v.end());
-    std::cout << one(arr) << "\n";
+    //std::cout << one(arr) << "\n";
     std::cout << two(arr) << "\n";
     return 0;
 }
